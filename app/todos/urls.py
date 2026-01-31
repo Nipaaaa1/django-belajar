e4ba9e2a98d9b0747de113b3ba57ef8daab224a7
Nipaaaa1 <frostone224@gmail.com>
2026-01-31 06:40:51 +0000

    feat: add form post example

--- app/todos/urls.py
+++ app/todos/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('hello', views.hello_world_view, name="Hello World"),
     path('hello/query/', views.hello_from_path, name="Hello from Path"),
+    path('post/example/', views.post_example, name="post_example"),
+    path('post/submit/', views.post_submit, name="Post Submit Example"),
 ]
--- app/todos/views.py
+++ app/todos/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotAllowed
 from django.shortcuts import render
 
 def hello_world_view(request):
@@ -8,3 +8,16 @@ def hello_from_path(request):
     name = request.GET.get("q")
 
     return HttpResponse(f"Hello {name}!")
+
+def post_example(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        age = request.POST.get("age")
+        job = request.POST.get("job")
+
+        return HttpResponse(f"You input data: {name}, {age}, {job}")
+    else:
+        return HttpResponseNotAllowed("POST")
+
+def post_submit(request):
+    return render(request, "todos/post_submit.html")
