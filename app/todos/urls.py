39a04786f9c389ae9033771e7db44fccf67f3471
Nipaaaa1 <frostone224@gmail.com>
2026-01-31 06:19:35 +0000

    feat: add dynamic path with query

--- app/todos/urls.py
+++ app/todos/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('hello', views.hello_world_view, name="Hello World"),
+    path('hello/query/', views.hello_from_path, name="Hello from Path"),
 ]
--- app/todos/views.py
+++ app/todos/views.py
@@ -1,4 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello_world_view(request):
     return render(request, "todos/hello.html")
+
+def hello_from_path(request):
+    name = request.GET.get("q")
+
+    return HttpResponse(f"Hello {name}!")
